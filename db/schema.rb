6b567912f7cfd0ed5458591f58f67beb5de59c8e
fmontei <felipe.carneiro.monteiro@gmail.com>
2014-11-18 18:00:10 -0500

    Updated student main page, added valid information and some redirects.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116232420) do
+ActiveRecord::Schema.define(version: 20141118222744) do
 
   create_table "bids", force: true do |t|
     t.integer  "team_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'John', last_name: 'Smith', team_id: '1') 
+Student.create(first_name: 'Sally', last_name: 'Sue', team_id: '1') 
+Student.create(first_name: 'Billy', last_name: 'Bob', team_id: '1') 
+Student.create(first_name: 'Hot', last_name: 'Dog', team_id: '1') 
